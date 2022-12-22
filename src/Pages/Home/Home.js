361bed88cfcc465c683cca5dca0f91bee95c8231
kyotodevindie <kyotodevindie@gmail.com>
2022-12-21 23:40:19 -0300

    feat: Home page

---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,10 @@
+import Layout from '../../Components/Organisms/Layout';
+import PlanCalculator from '../../Components/Organisms/PlanCalculator/PlanCalculator';
+
+export default function Home() {
+  return (
+    <Layout>
+      <PlanCalculator />
+    </Layout>
+  );
+}
